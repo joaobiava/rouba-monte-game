93c044608764bfbc51d9448a14340743d245fefe
João antonio Biava <jab-biava@hotmail.com>
2024-05-23 21:14:17 -0300

    ai

--- fudidinho.cpp
+++ fudidinho.cpp
@@ -6,8 +6,7 @@
 
 using namespace std;
 
-struct Carta
-{
+struct Carta{
     string naipe;
     int numero;
 };
@@ -71,6 +70,7 @@ vector<Carta> distribuirCartas(stack<Carta> &baralho){
 }
 
 void print(vector<Carta> &cartasMesa, vector<Carta> &cartasJogador1, vector<Carta> &cartasJogador2){
+    system("cls");
     cout << endl << "--------------------------------------" << endl;
 
     cout << "Cartas na Mesa: " << endl;
@@ -148,6 +148,7 @@ void vezJogadores(vector<Carta> &cartasJogador1, vector<Carta> &cartasMesa, stac
                 cartasJogador1 = distribuirCartas(baralho);
             }
             if(!cartasJogador1.empty()){
+                cout << "vez do jogador 1:" << endl;
                 jogada(cartasJogador1, cartasMesa, monteJogador1);
             }
         } else{
@@ -155,6 +156,7 @@ void vezJogadores(vector<Carta> &cartasJogador1, vector<Carta> &cartasMesa, stac
                 cartasJogador2 = distribuirCartas(baralho);
             }
             if(!cartasJogador2.empty()){
+                cout << "vez do jogador 2" << endl;
                 jogada(cartasJogador2, cartasMesa, monteJogador2);
             }
         }
